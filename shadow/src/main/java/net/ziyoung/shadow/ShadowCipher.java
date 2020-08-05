aac8bafd951361d4b5cc92e87ae86fd1113ce5be
zhiyang <dengzhiyang123@126.com>
2020-08-05 22:44:14 +0800

    feat: add meta cipher

--- shadow/src/main/java/net/ziyoung/shadow/ShadowCipher.java
+++ shadow/src/main/java/net/ziyoung/shadow/ShadowCipher.java
@@ -1,17 +1,13 @@
 package net.ziyoung.shadow;
 
-import com.google.crypto.tink.Aead;
-
-import java.security.GeneralSecurityException;
-
 public interface ShadowCipher {
 
     int keySize();
 
     int saltSize();
 
-    Aead encrypter(byte[] salt) throws GeneralSecurityException;
+    byte[] encrypt(byte[] plaintext, byte[] iv) throws Exception;
 
-    Aead decrypter(byte[] salt);
+    byte[] decrypt(byte[] ciphertext, byte[] iv) throws Exception;
 
 }
